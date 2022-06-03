c08f800abdf682dd0c511cf89cb49f915bcad0b5
cagatayyildiz <yildiz.cagatay@hotmail.com>
2022-06-03 12:17:17 +0300

    react hook form sample

--- App.js
+++ App.js
@@ -1,19 +1,20 @@
 
 //class base component, function base component
 
-import {SafeAreaView } from 'react-native'
+import { SafeAreaView } from 'react-native'
 import React from 'react'
 import AddProduct from './src/fetchSample/AddProduct'
 import ProductList from './src/fetchSample/ProductList'
 import AddSupplier from './src/formikSample/AddSupplier'
+import AddCategory from './src/hookFormSample/AddCategory'
 
 const App = () => {
-  
+
   return (<>
-    <SafeAreaView style={{flex:1}}>
-      <AddSupplier></AddSupplier>
+    <SafeAreaView style={{ flex: 1 }}>
+      <AddCategory></AddCategory>
     </SafeAreaView>
-  
+
   </>
 
   )
--- src/hookFormSample/AddCategory.js
+++ src/hookFormSample/AddCategory.js
@@ -1,12 +1,83 @@
-import { View, Text } from 'react-native'
+import { View, Text, TextInput, StyleSheet, Button } from 'react-native'
 import React from 'react'
+import { Controller, useForm } from 'react-hook-form'
+import axios from 'axios';
+
+
 
 const AddCategory = () => {
-  return (
-    <View>
-      <Text>AddCategory</Text>
-    </View>
-  )
+
+    const { control, handleSubmit, formState: { errors } } = useForm({
+        defaultValues: {
+            name: '',
+            description: ''
+        }
+    });
+
+    const onSubmit = (data) => {
+
+        axios.post('https://northwind.vercel.app/api/categories', data)
+            .then((res) => {
+                console.log('RESPONSE', res.data);
+            })
+
+    }
+
+
+    return (
+        <View>
+            <Controller
+                control={control}
+                rules={{
+                    required: true,
+                }}
+                render={({ field: { onChange, onBlur, value } }) => (
+                    <TextInput
+                        style={styles.textInput}
+                        onBlur={onBlur}
+                        onChangeText={onChange}
+                        value={value}
+                    />
+                )}
+                name="name"
+            >
+
+            </Controller>
+
+            {errors.name && <Text style={{ color: 'tomato' }}>This is required.</Text>}
+
+
+            <Controller
+                control={control}
+                rules={{
+                    required: true,
+                }}
+                render={({ field: { onChange, onBlur, value } }) => (
+                    <TextInput
+                        style={styles.textInput}
+                        onBlur={onBlur}
+                        onChangeText={onChange}
+                        value={value}
+                    />
+                )}
+                name="description"
+            >
+
+            </Controller>
+
+            <Button title="Submit" onPress={handleSubmit(onSubmit)} />
+
+        </View>
+    )
 }
+
+const styles = StyleSheet.create({
+    textInput: {
+        height: 40,
+        margin: 12,
+        borderWidth: 1,
+        padding: 10,
+    },
+});
 
 export default AddCategory
